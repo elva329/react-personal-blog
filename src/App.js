5f1f3fe18e5e070e583b9253bec2bd35cc6173e5
elva329 <jia_shen@126.com>
2020-03-06 19:34:09 +0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header';
 import store from './store/index';
 class App extends Component {
@@ -8,7 +9,19 @@ class App extends Component {
     return (
       <React.Fragment>
         <Provider store={store}>
-          <Header />
+          <div>
+            <Header />
+            <BrowserRouter>
+              <div>
+                <Route path="/" exact render={() => <div>home</div>}></Route>
+                <Route
+                  path="/detail"
+                  exact
+                  render={() => <div>detail</div>}
+                ></Route>
+              </div>
+            </BrowserRouter>
+          </div>
         </Provider>
       </React.Fragment>
     );
