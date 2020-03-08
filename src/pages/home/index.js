908e9a72dfa72399ff788557d38c68b365181ec7
elva329 <jia_shen@126.com>
2020-03-08 17:18:22 +0800

    optimized code

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,8 +5,8 @@ import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writer from './components/Writer';
-import axios from 'axios';
-import { matchPath } from 'react-router-dom';
+import { actionCreators } from './store';
+
 class Home extends Component {
   state = {};
   render() {
@@ -29,21 +29,13 @@ class Home extends Component {
     );
   }
   componentDidMount() {
-    axios.get('/api/home.json').then(res => {
-      const result = res.data.data;
-      const action = {
-        type: 'change_home_data',
-        topicList: result.topicList,
-        articleList: result.articleList,
-        recommendList: result.recommendList
-      };
-      this.props.changeHomeData(action);
-    });
+    this.props.changeHomeData();
   }
 }
 
 const mapDispatch = dispatch => ({
-  changeHomeData(action) {
+  changeHomeData() {
+    const action = actionCreators.getHomeInfo();
     dispatch(action);
   }
 });
