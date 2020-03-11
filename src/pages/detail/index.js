87d9eb00877a15a9102f44bc432871197cf82d53
elva329 <jia_shen@126.com>
2020-03-11 14:39:23 +0800

    pass parameters to the detail url

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -4,15 +4,16 @@ import { DetailWrapper, Header, Content } from './style';
 import { actionCreators } from './store';
 class Detail extends Component {
   render() {
+    console.log(this.props);
     return (
       <DetailWrapper>
         <Header>{this.props.title}</Header>
-        <Content dangerouslySetInnerHTML={{ __html: this.props.connect }} />
+        <Content dangerouslySetInnerHTML={{ __html: this.props.content }} />
       </DetailWrapper>
     );
   }
   componentDidMount() {
-    this.props.getDetail();
+    this.props.getDetail(this.props.match.params.id);
   }
 }
 
@@ -23,8 +24,8 @@ const mapState = state => ({
 });
 
 const mapDispatch = dispatch => ({
-  getDetail() {
-    dispatch(actionCreators.getDetail());
+  getDetail(id) {
+    dispatch(actionCreators.getDetail(id));
   }
 });
 export default connect(mapState, mapDispatch)(Detail);
